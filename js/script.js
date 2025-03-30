2e86327d6138e3bedcd40fa4eae8704578852eac
Vinny Rock <tamuravini11@gmail.com>
2025-03-30 11:56:54 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,5 +5,37 @@ $(document).ready (function(){
 	});
 
 	
-	
+	$('.owl-carousel').owlCarousel({
+	    loop:false,
+	    margin:10,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:4
+	        }
+	    }
 });
+
+$('.chart').easyPieChart({
+            easing: 'easeInOut',
+            barColor:'#fff',
+            trackColor: false,
+            scaleColor: false,
+            lineWidth: 4,
+            size: 152,
+            onstep:function(from,to,percent) {
+            	$(this.el).find('.percent').text(Math.round(percent));
+            }
+
+
+        });
+
+
+
+    });	
